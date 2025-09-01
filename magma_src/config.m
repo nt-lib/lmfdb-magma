56421a66e3195fa18b0ba06093eae4de220b22a0
Maarten Derickx <maarten@mderickx.nl>
2025-09-01 20:36:26 +0000

    Improve configuration variable loading and storing

--- magma_src/config.m
+++ magma_src/config.m
@@ -1,66 +1,20 @@
-/* the below doesn't work I guess magma copies on return or something
-// Global variable to store the settings in
-config := AssociativeArray(); 
-config["python_interpreter_path"] := "python";
-
-intrinsic MDGetConfig() -> Assoc
-{Set the python path to the python interpreter for use by LMFDB-magma}
-    return config;
-end intrinsic;
-
-intrinsic MDSetPythonInterpreter(path::MonStgElt)
-{Set the python path to the python interpreter for use by LMFDB-magma}
-    config := MDGetConfig();
-    config["python_interpreter_path"] := path;
-end intrinsic;
+// A global variable to store all the data in
+config := NewStore();
+StoreSet(config, "python_interpreter_path", "python");
 
 
-intrinsic MDGetPythonInterpreter() -> MonStgElt
-{Set the python path to the python interpreter for use by LMFDB-magma}
-    return config["python_interpreter_path"];
-end intrinsic;
-*/
-
-/* this doesn't work either for the same reason
-config := ["python"];
-
-intrinsic MDGetConfig() -> SeqEnum
-{Set the python path to the python interpreter for use by LMFDB-magma}
-    return config;
-end intrinsic;
-
-intrinsic MDSetPythonInterpreter(path::MonStgElt)
-{Set the python path to the python interpreter for use by LMFDB-magma}
-    config := MDGetConfig();
-    config[1] := path;
-end intrinsic;
-
-
-intrinsic MDGetPythonInterpreter() -> MonStgElt
-{Set the python path to the python interpreter for use by LMFDB-magma}
-    return config[1];
-end intrinsic;
-*/
-
-// So we go with a super hacky solution and use magmas AssignNames
-// functionality to store strigns globally.
-config := FreeGroup(1); 
-AssignNames(~config, ["python"]);
-
-
-intrinsic MDGetConfig() -> GrpFP
-{A free group whose variable names store the configuration variables}
+intrinsic MDGetConfig() -> Rec
+{A record containing all configuration variables}
     return config;
 end intrinsic;
 
 intrinsic MDSetPythonInterpreter(path::MonStgElt)
 {Set the path to the python interpreter for use by LMFDB-magma}
-    config := MDGetConfig();
-    AssignNames(~config, [path]);
+    StoreSet(config, "python_interpreter_path", path);
 end intrinsic;
 
 
 intrinsic MDGetPythonInterpreter() -> MonStgElt
 {Get the path to the python interpreter used by LMFDB-magma}
-    return Sprint(Name(config,1));
+    return StoreGet(config, "python_interpreter_path");
 end intrinsic;
